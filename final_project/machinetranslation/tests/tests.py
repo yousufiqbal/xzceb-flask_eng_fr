319890ba4eb2be6a8e978ac6a10181c3467d8db2
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-12-22 22:50:57 +0500

    ..

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -4,11 +4,17 @@ from translator import english_to_french, french_to_english
 class TestTranslationFunctions(unittest.TestCase):
 
     def test_english_to_french(self):
-        self.assertEqual(english_to_french(), None)
+        # null check (nothing provided)
+        self.assertEqual(english_to_french(), '')
+        # null check (provided empty string '')
+        self.assertEqual(english_to_french(''), '')
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
     
     def test_french_to_english(self):
-        self.assertEqual(french_to_english(), None)
+        # null check (nothing provided)
+        self.assertEqual(french_to_english(), '')
+        # null check (provided empty string '')
+        self.assertEqual(french_to_english(''), '')
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
 
 if __name__ == '__main__':
